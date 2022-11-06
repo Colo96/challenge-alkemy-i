b16cf53a64be89a1c3eeb20e027fe365eb3d1087
Axel Langerman <solomonsilversmith@gmail.com>
2022-11-06 17:00:24 -0300

    Preparando challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Login from './components/Login';
 import Listado from './components/Listado';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Header/>
       <Routes>
         <Route exact path='/' element={<Login/>}/>
         <Route path='/listado' element={<Listado/>}/>
       </Routes>
+      <Footer/>
     </>
   );
 }
